f145132d09790a80a3fe3636d994af300f515b4e
10907000@live.mercer.edu <Kyle Jones>
2014-09-05 17:57:56 -0400

    homework week_3

---
+++ homework /week_3/pine2.5.rb
@@ -0,0 +1,14 @@
+#hours in a year
+    puts 24 * 365
+
+#if it is a leap year
+    puts 24 * 366
+
+#minutes in a decade
+    puts 60 * 24 * (365 * 10 + 2)
+
+#age in seconds (60 * 60 * 24)=86400
+    puts 86400 * 365 * 22
+
+#authors age (1,160,000,000) / (60 * 60 * 24 * 365)
+    puts 1,160,000,000 / (3600 * 8760) 
